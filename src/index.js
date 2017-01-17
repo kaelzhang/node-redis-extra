1f68145de444b0098974d6ba15f629260f7b3280
Kael Zhang <i@kael.me>
2017-01-17 15:29:38 +0800

    main; install scripts for travis; ignores redis db file

---
+++ src/index.js
@@ -0,0 +1,42 @@
+module.exports = redis_extra
+
+const {RedisClient} = require('redis')
+const commands = require('redis-commands')
+const thenify = require('thenify')
+
+const redis_config = require('../config').redis
+
+const client = redis.createClient(redis_config).on('error', e => {
+  console.error(`Fails to connect to redis server.\n\n${e.stack || e}`)
+  process.exit(1)
+})
+
+
+class RedisExtra extends RedisClient {
+  constructor (options) {
+    super(options)
+  }
+}
+
+
+function redis_extra (options) {
+  return new RedisExtra(options)
+}
+
+redis_extra.RedisExtra = RedisExtra
+
+
+function normalize_redis_command (command) {
+  return command.replace(/(?:^([0-9])|[^a-zA-Z0-9_$])/g, '_$1')
+}
+
+commands.forEach(command => {
+  command = normalize_redis_command(command)
+
+  const method = RedisExtra.prototype[command]
+  if (!method) {
+    return
+  }
+
+  RedisExtra.prototype[command] = thenify(method)
+})
